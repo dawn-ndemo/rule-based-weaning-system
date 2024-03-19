456c74fc25496ada67894c25f7d99e41683f14b8
dawn-ndemo <dawnmoraa@gmail.com>
2024-03-19 15:10:22 +0300

    update CORS

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://localhost:3000"], headers=['Content-Type']) 
 
 @app.route('/process-data', methods=['POST'])
 def process_data():
