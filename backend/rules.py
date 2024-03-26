4eabbd82af6a0c4e869a0316582e105eb82e53a6
dawn-ndemo <dawnmoraa@gmail.com>
2024-03-26 14:54:01 +0300

    replace print with self-declare

--- backend/rules.py
+++ backend/rules.py
@@ -7,21 +7,21 @@ class FeedingExpert(KnowledgeEngine):
 
     @Rule(FeedingGuideline(age="6-8 months"))
     def feed_6_to_8_months(self):
-        print("6-8 months: Introduce thick porridge, mashed or pureed foods")
-        print("Frequency: 2-3 meals a day, frequent breastfeeding")
-        print("Amount: 2-3 tablespoons to half a 250 ml cup")
+        self.declare("6-8 months: Introduce thick porridge, mashed or pureed foods")
+        self.declare("Frequency: 2-3 meals a day, frequent breastfeeding")
+        self.declare("Amount: 2-3 tablespoons to half a 250 ml cup")
 
     @Rule(FeedingGuideline(age="9-11 months"))
     def feed_9_to_11_months(self):
-        print("9-11 months: Introduce finely chopped, diced or mashed foods")
-        print("Frequency: 3-4 meals a day, 1-2 snacks")
-        print("Amount: half to three quarters of a 250ml cup")
+        self.declare("9-11 months: Introduce finely chopped, diced or mashed foods")
+        self.declare("Frequency: 3-4 meals a day, 1-2 snacks")
+        self.declare("Amount: half to three quarters of a 250ml cup")
 
     @Rule(FeedingGuideline(age="12-23 months"))
     def feed_12_to_23_months(self):
-        print("12-23 months: Introduce chopped into small pieces or mashed if needed")
-        print("Frequency: 3-4 meals a day, 1-2 snacks")
-        print("Amount: three quarters to full 250ml cup")
+        self.declare("12-23 months: Introduce chopped into small pieces or mashed if needed")
+        self.declare("Frequency: 3-4 meals a day, 1-2 snacks")
+        self.declare("Amount: three quarters to full 250ml cup")
 
     def get_recommendations(self, age_in_months):
         self.reset()
